df550cdeaa5a87c58103d24d230d686688423a7c
Drew Hill <mdude2314@gmail.com>
2019-05-21 17:53:24 -0400

    Add countdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,9 +49,12 @@ class BugFactory {
 class Game {
   squashCount = 0
   bugs = []
-  gameDurationSeconds = 5
+  gameDurationSeconds = 30
+  gameDurationRemaining = this.gameDurationSeconds;
   gameContainer = null
+  gameCountdown = null;
   gameInterval = null
+  countdownInterval = null;
   bugFactory = null
 
   async start() {
@@ -61,6 +64,14 @@ class Game {
     this.gameContainer = this.createContainer()
     this.bugFactory = new BugFactory(height, width, this.squashCallback)
     await this.showIntro()
+    this.gameCountdown = this.createCountdown();
+    this.gameContainer.appendChild(this.gameCountdown);
+
+    this.countdownInterval = setInterval(() => {
+      this.gameDurationRemaining--;
+      this.gameCountdown.innerText = this.gameDurationRemaining;
+    }, 1000)
+
     this.gameInterval = setInterval(() => {
       const bugElement = this.bugFactory.createBugElement()
       this.bugs.push(bugElement)
@@ -74,6 +85,7 @@ class Game {
 
   async showIntro() {
     return new Promise(async resolve => {
+      await this.doIntroText("You found a bug! Squash as many more as you can!", 3000)
       await this.doIntroText("3...")
       await this.doIntroText("2...")
       await this.doIntroText("1...")
@@ -82,7 +94,7 @@ class Game {
     })
   }
 
-  doIntroText(text) {
+  doIntroText(text, screenDuration = 750) {
     return new Promise((resolve, reject) => {
       const element = document.createElement("h2")
       element.innerText = text
@@ -94,19 +106,28 @@ class Game {
           element.remove()
           resolve()
         }, 250)
-      }, 750)
+      }, screenDuration)
     })
   }
 
   createContainer() {
     const container = document.createElement("div")
     container.classList.add("game-container")
-    document.body.appendChild(container)
+    document.body.appendChild(container);
     return container
+  }
+
+  createCountdown() {
+    const countdown = document.createElement("div");
+    countdown.innerHTML = this.gameDurationRemaining;
+    countdown.classList.add("game-countdown");
+    return countdown;
   }
 
   end() {
     clearInterval(this.gameInterval)
+    clearInterval(this.countdownInterval);
+    this.gameDurationRemaining = this.gameDurationSeconds;
     this.bugs.forEach(bug => this.bugFactory.animateSquash(bug))
     console.log(`you squashed ${this.squashCount} bugs!`)
     setTimeout(() => {
